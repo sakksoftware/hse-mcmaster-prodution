52cdd2f5f80edd92032a7655b7cade0340e1b000
Michael Yagudaev <michael@yagudaev.com>
2015-07-06 14:06:39 -0400

    quick attempt of deployment using koa instead of statik

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-var statik = require('statik');
+// var statik = require('statik');
 port = process.env.PORT || 3000
-statik(port);
+// statik(port);
+// console.log("starting server on port ", port)
+
+var koa = require('koa');
+var app = koa();
+
+app.use(function *(){
+  this.body = 'Hello World';
+});
+
+app.listen(process.env.PORT || 3000);
 console.log("starting server on port ", port)
