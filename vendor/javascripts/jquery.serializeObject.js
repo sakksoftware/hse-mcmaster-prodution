934b2b5758bd7b84bff54b58018d123df55b56ec
Michael Yagudaev <michael@yagudaev.com>
2015-09-02 19:37:06 -0400

    change json form serialization to add `true` value to checkboxes instead of `on`.

--- vendor/javascripts/jquery.serializeObject.js
+++ vendor/javascripts/jquery.serializeObject.js
@@ -3,6 +3,11 @@ $.fn.serializeObject = function()
     var o = {};
     var a = this.serializeArray();
     $.each(a, function() {
+        // is this a checkbox?
+        if (this.value == 'on') {
+          this.value = true
+        }
+
         if (o[this.name] !== undefined) {
             if (!o[this.name].push) {
                 o[this.name] = [o[this.name]];
