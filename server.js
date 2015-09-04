323f6cd19e4d2dbdbc99f8a8d105a28958fd2afa
Michael Yagudaev <michael@yagudaev.com>
2015-09-03 20:33:10 -0400

    add article url mapper

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ var basicAuth = require('basic-auth-connect');
 app.use(basicAuth('hse', 'withgreatpower'));
 
 app.use(express.static('public'));
+
+app.get('/one-page-summary.aspx', function(req, res) {
+  id = req.url.match(/one-page-summary\.aspx\?A=([0-9]+)&T=/)[1];
+  console.log('redirecting to ' + '/articles/' + id)
+  res.redirect('/articles/' + id);
+});
+
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname,'public/index.html'));
 });
