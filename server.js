8388311db51b2cfc56d228470284a44078590a99
Michael Yagudaev <michael@yagudaev.com>
2015-12-23 10:21:45 -0800

    move mocks into express server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var crypto = require('crypto');
 var rollbar = require('rollbar');
 var compression = require('compression');
 var logger = require('morgan');
+var bodyParser = require('body-parser')
 
 var NODE_ENV = process.env.NODE_ENV || 'development';
 var AES_KEY = process.env.AES_KEY || '';
@@ -38,6 +39,7 @@ if(NODE_ENV == 'production' || NODE_ENV == 'staging') {
 
 app.use(logger('dev'));
 app.use(compression());
+app.use(bodyParser.json());
 app.use(rollbar.errorHandler(ROLLBAR_SERVER_SECRET));
 app.use(express.static('public'));
 
--- server/mocks/routes.js
+++ server/mocks/routes.js
@@ -36,6 +36,116 @@ module.exports = function(app) {
     res.json(data);
   });
 
+  app.get('/api/user/curated_searches', function(req, res) {
+    var curated_searches = [
+      {
+        "applied_filters": ["22_31"],
+        "filters": [
+          {
+            "applied": true,
+            "id": "22_31",
+            "title": "Aboriginal health"
+          },
+        ],
+        "id": 1,
+        "query": "HIV Positive",
+        "saved": false,
+        "subscribed": false,
+        "userid": 0
+      }
+    ];
+
+    filter = curated_searches[0].filters[0];
+    var filters = [];
+    for(var i = 0; i < 10; i++) {
+      filters.push(filter);
+    }
+    curated_searches[0].filters = filters;
+
+    res.json(curated_searches);
+  });
+
+  app.post('/api/user/searches', function(req, res) {
+    var params = req.body;
+    var saved_search = {
+      "applied_filters": ["22_31"],
+      "filters": [
+        {
+          "applied": true,
+          "id": "22_31",
+          "title": "Aboriginal health"
+        }
+      ],
+      "id": 35,
+      "query": "HIV Positive",
+      "saved": true,
+      "subscribed": params.subscribed,
+      "userid": 0
+    };
+    res.json(saved_search);
+  });
+
+  app.post('/api/user/searches/:id/subscribe', function(req, res) {
+    var saved_search = {
+      "applied_filters": ["22_31"],
+      "filters": [
+        {
+          "applied": true,
+          "id": "22_31",
+          "title": "Aboriginal health"
+        }
+      ],
+      "id": 35,
+      "query": "HIV Positive",
+      "saved": true,
+      "subscribed": true,
+      "userid": 0
+    };
+
+    res.json(saved_search);
+  });
+
+  app.post('/apiuser/searches/:id/unsubscribe', function(req, res) {
+    var saved_search = {
+      "applied_filters": ["22_31"],
+      "filters": [
+        {
+          "applied": true,
+          "id": "22_31",
+          "title": "Aboriginal health"
+        }
+      ],
+      "id": 35,
+      "query": "HIV Positive",
+      "saved": true,
+      "subscribed": false,
+      "userid": 0
+    };
+    res.json(saved_search);
+  });
+
+  app.post('/api/user/searches/remove', function(req, res) {
+    var saved_search = [
+      {
+        "applied_filters": ["22_31"],
+        "filters": [
+          {
+            "applied": true,
+            "id": "22_31",
+            "title": "Aboriginal health"
+          }
+        ],
+        "id": 35,
+        "query": "HIV Positive",
+        "saved": false,
+        "subscribed": false,
+        "userid": 0
+      }
+    ];
+
+    res.json(saved_search);
+  });
+
   function markFilters(filters, appliedFilters) {
     return filters.map(function(filter) {
       var f = appliedFilters.find(function(f) { return f === filter.id || (typeof f === 'object' && f.id === filter.id) });
