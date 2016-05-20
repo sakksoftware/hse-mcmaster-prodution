955b0863f9e97ba0b3575e9b09a293d48642144d
Michael Yagudaev <michael@yagudaev.com>
2016-05-20 16:46:07 -0700

    fix script and add install step for the cli tool

--- switch_sync.js
+++ switch_sync.js
@@ -1,11 +1,10 @@
-var sys = require('sys');
 var exec = require('child_process').exec;
 
 child = exec('switch csv2json hse.csv ./tmp/locales', function(error, stdout, stderror) {
-  sys.print('stdout: ' + stdout);
-  sys.print('stderr: ' + stderr);
+  console.log('stdout: ' + stdout);
+  console.log('stderr: ' + stderr);
 
-  if (error) return sys.print('Fatal error executing switch command');
+  if (error) return console.log('Fatal error executing switch command');
 
-  sys.print('Successfully synced locale files');
+  console.log('Successfully synced locale files');
 });
