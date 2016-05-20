a852b75ee035b1bf3c77a499fdbd7cf6376d4e63
Michael Yagudaev <michael@yagudaev.com>
2016-05-20 16:41:16 -0700

    add sync translations task (will need some modifications like writing to s3 instead of local file system temp folder

---
+++ switch_sync.js
@@ -0,0 +1,11 @@
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+child = exec('switch csv2json hse.csv ./locales', function(error, stdout, stderror) {
+  sys.print('stdout: ' + stdout);
+  sys.print('stderr: ' + stderr);
+
+  if (error) return sys.print('Fatal error executing switch command');
+
+  sys.print('Successfully synced locale files');
+});
